8445b40a958cd8b8f264212af4740e1223c4000f
joenjonghyun <jjh980814@naver.com>
2022-04-20 19:27:27 +0900

    22.4.20.board해야함

--- pages/basic/api/basicAPI.js
+++ pages/basic/api/basicAPI.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 const SERVER = `http://localhost:5000`
 
-export const basicBmi = bmiRequest => axios.post(`${SERVER}/member/bmi`, bmiRequest)
-export const basicCalc = calcRequest => axios.post(`${SERVER}/member/calc`, calcRequest)
+export const basicBmi = bmiRequest => axios.post(`${SERVER}/basic/bmi`, bmiRequest)
+export const basicCalc = calcRequest => axios.post(`${SERVER}/basic/calc`, calcRequest)
